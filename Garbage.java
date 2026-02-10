198ec50b2ea8ae18e6366b579807a64efd824912
Divyansh Jindal <divyanshjindal01@gmail.com>
2026-02-10 14:29:36 +0530

    garbage

---
+++ Garbage.java
@@ -0,0 +1,10 @@
+public class Garbage {
+    public static void main(String[] args) {
+        int a, b, c;
+        b = 10;
+        c = 10;
+        System.out.println(b);
+        System.gc();
+    }
+    
+}
