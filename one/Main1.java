a5ce0c7a4c05347981e020e0143fd1a59f53de03
Divyansh Jindal <divyanshjindal01@gmail.com>
2026-02-12 08:51:05 +0530

    packages

---
+++ one/Main1.java
@@ -0,0 +1,14 @@
+package one;
+
+class Car{
+    int milage;
+    int price;
+}
+public class Main1 {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.milage=10;
+        c.price=100;
+        System.out.println(c.milage);
+    }
+}
---
+++ one/Main2.java
@@ -0,0 +1,10 @@
+package one;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.milage=20;
+        System.out.println(c.milage);
+    }
+    
+}
